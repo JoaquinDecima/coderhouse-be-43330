77af34bc844d69588e5c79061e822195afc726bc
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-04-04 13:30:12 -0300

    Add comments logs

--- Clase 01 - Introduccion/logs.js
+++ Clase 01 - Introduccion/logs.js
@@ -1,7 +1,14 @@
-let numero = 1;
-console.log(numero);
-numero = 'uno';
-console.log(numero);
+// Log de un Numero
+console.log(1);
+
+// Log de un String
+console.log('uno');
+
+// Log de un booleano
 console.log(true);
+
+// Log de Null
 console.log(null);
+
+// Log de undefined
 console.log(undefined);
