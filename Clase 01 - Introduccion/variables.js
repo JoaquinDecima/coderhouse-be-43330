881b64bfb6e37be3574edf34d254b2b0d9a1cfe7
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-04-04 13:40:01 -0300

    Add variables.js

---
+++ Clase 01 - Introduccion/variables.js
@@ -0,0 +1,23 @@
+// Creo variables con los datos solicitados
+let nombre = 'Pato';
+let edad = 26;
+let precio = 1578.5;
+let series = ['Game of thrones', 'Dr. House', 'Mr. Robot'];
+
+// Muestro en terminal el contenido de las variables
+console.log(nombre);
+console.log(edad);
+console.log(precio);
+console.log(series);
+
+// Incremento edad en 1
+edad = edad + 1;
+// edad += 1;
+// edad++;
+
+// Agrego una serie
+series.push('Dexter');
+
+// Vuelvo a imprimir los datos modificados
+console.log(edad);
+console.log(series);
