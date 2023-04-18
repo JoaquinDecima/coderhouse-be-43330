51a90e9365739d12f0f2b584d9ca91737257e34a
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-04-18 09:44:34 -0300

    add comments to fsSync

--- Clase 04 - Manejo de Archivos/fsSync.js
+++ Clase 04 - Manejo de Archivos/fsSync.js
@@ -1,15 +1,26 @@
+// Llamo a la libreria fs y la guardo en variable fs
 const fs = require('fs');
 
-fs.writeFileSync('./files/fsSync.txt', 'Estoy escribiendo un Archivo \n');
+/* Escribo el archivo './fsSync.txt' poniendo dentro
+ * 'Estoy escribiendo un Archivo \n' de manera sincronica
+ */
+fs.writeFileSync('./fsSync.txt', 'Estoy escribiendo un Archivo \n');
 
-if (fs.existsSync('./files/fsSync.txt')) {
-	let contenido = fs.readFileSync('./files/fsSync.txt', 'utf-8');
-	console.log(contenido);
+/* Pregunto de manera sincronica si existe el archivo
+ * './fsSync.txt', esto retorna un booleano
+ */
+if (fs.existsSync('./fsSync.txt')) {
+	// Guardo en la variable contenido, la lectura del archivo.
+	let contenido = fs.readFileSync('./fsSync.txt', 'utf-8');
+	console.log(contenido); // Imprimo en pantalla
 
-	fs.appendFileSync('./files/fsSync.txt', 'Aca agrego algo mas');
+	// Agrego al archivo './fsSync.txt' el texto 'Aca agrego algo mas'
+	fs.appendFileSync('./fsSync.txt', 'Aca agrego algo mas');
 
-	contenido = fs.readFileSync('./files/fsSync.txt', 'utf-8');
-	console.log(contenido);
+	// Guardo nuevamente en la variable contenido, la lectura del archivo.
+	contenido = fs.readFileSync('./fsSync.txt', 'utf-8');
+	console.log(contenido); // Imprimo en pantalla
 
-	fs.unlinkSync('./files/fsSync.txt');
+	// Elimino el archivo
+	fs.unlinkSync('./fsSync.txt');
 }
