f3ba2462e27de54b928ab038cc0896a5e09e7db1
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-07-20 10:31:26 -0300

    update front

--- Clase 29 - Servidor en Capas/public/js/main.js
+++ Clase 29 - Servidor en Capas/public/js/main.js
@@ -1,11 +1,13 @@
-async function getOrders(){
-    const response = await fetch('/api/orders');
-    return await response.json();
+function getOrders(){
+    fetch('/api/orders')
+        .then(response => response.json())
+        .then(orders => {
+            drawOrders(orders.payload);
+        });
 }
 
-async function drawOrders(){
-    const ordersRow = document.getDocumentById('orders');
-    const orders = await getOrders();
+function drawOrders(orders){
+    const ordersRow = document.getElementById('orders');
     let html = '';
 
     orders.forEach(order => {
@@ -13,22 +15,25 @@ async function drawOrders(){
             <div class="col-4">
                 <div class="card text-center">
                     <div class="card-header">
-                        El vendedor Feliz
+                        ${order.business}
                     </div>
                     <div class="card-body">
                         <h5 class="card-title">Orden Numero: ${order.number}</h5>
-                        <p class="card-text">Este pedido fue realizado por el usuario PatoJAD y contiene los siguientes
+                        <p class="card-text">Este pedido fue realizado por el usuario ${order.user} y contiene los siguientes
                             productos:</p>
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">1</li>
-                        <li class="list-group-item">6</li>
+                        ${order.products.map(product => `<li class="list-group-item">${product}</li>`).join('')}
                     </ul>
                     <div class="card-footer text-body-secondary">
-                        $ 4500
+                        $ ${order.totalPrice}
                     </div>
                 </div>
             </div>
         `;
     });
+
+    ordersRow.innerHTML = html;
 }
+
+getOrders();
