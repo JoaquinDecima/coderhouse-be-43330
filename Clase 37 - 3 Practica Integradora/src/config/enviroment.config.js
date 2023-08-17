e92bf9bd88484ab37cb576a68f5219fc76f300ab
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-08-16 22:22:44 -0300

    implement singletton

--- Clase 37 - 3 Practica Integradora/src/config/enviroment.config.js
+++ Clase 37 - 3 Practica Integradora/src/config/enviroment.config.js
@@ -2,35 +2,50 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const persistence = process.env.PERSISTENCE || 'mongodb';
-let userDAO;
-let courseDAO;
+class ConfigManager {
+    static #instance = null;
+    constructor() {
+        if (!ConfigManager.#instance) {
+            ConfigManager.#instance = this;
+            this.PORT = process.env.PORT || 8080;
+            this.MONGO_URI = process.env.MONGO_URI;
+            this.PERSISTENCE = process.env.PERSISTENCE || 'mongodb';
+            this.userDAO = null;
+            this.courseDAO = null;
+            this.initializePersistence();
+        } else {
+            return ConfigManager.#instance;
+        }
+    }
 
-if (persistence === 'mongodb') {
-    console.log(`Working with Database persistence in mongodb`);
-    await import('../dao/dbManagers/courses.js')
-        .then(module => {
-            courseDAO = new module.default();
-        })
-    await import('../dao/dbManagers/users.js')
-        .then(module => {
-            userDAO = new module.default();
-        })
-} else {
-    console.log(`Working with file persistence`);
-    await import('../dao/fileManagers/courses.js')
-        .then(module => {
-            courseDAO = new module.default();
-        })
-    await import('../dao/fileManagers/users.js')
-        .then(module => {
-            userDAO = new module.default();
-        })
+    async initializePersistence() {
+        if (this.PERSISTENCE === 'mongodb') {
+            console.log(`Working with Database persistence in mongodb`);
+            await import('../dao/dbManagers/courses.js')
+                .then(module => {
+                    this.courseDAO = new module.default();
+                })
+            await import('../dao/dbManagers/users.js')
+                .then(module => {
+                    this.userDAO = new module.default();
+                })
+        } else {
+            console.log(`Working with file persistence`);
+            await import('../dao/fileManagers/courses.js')
+                .then(module => {
+                    this.courseDAO = new module.default();
+                })
+            await import('../dao/fileManagers/users.js')
+                .then(module => {
+                    this.userDAO = new module.default();
+                })
+        }
+    }
 }
 
-export default {
-    PORT: process.env.PORT || 8080,
-    MONGO_URI: process.env.MONGO_URI,
-    userDAO: userDAO,
-    courseDAO: courseDAO
-}
+
+
+
+const config = new ConfigManager();
+
+export default config;
